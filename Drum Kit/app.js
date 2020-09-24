8e330dc19929de83fb9139bef788ef2214928c1a
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-09-25 01:03:35 +0530

    Third project

---
+++ Drum Kit/app.js
@@ -0,0 +1,18 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;  //skip if its not transform
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;   //stop the function from running altogether
+
+    key.classList.add('playing');
+    audio.currentTime = 0;    //rewind to the start
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
