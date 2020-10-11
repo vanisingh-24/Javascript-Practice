157915cc9c8f6a27d27a70254baa86c9927ee82a
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-10-11 14:57:14 +0530

    Added some other files

---
+++ Dynamic Checkboxes/app.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+    //check if they had the shift key down
+    //And check that they are checking it
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        //Go ahead 
+        //loop over every single checkbox
+        checkboxes.forEach(checkbox => {
+           if(checkbox === this || checkbox === lastChecked){
+               inBetween = !inBetween;
+               console.log('Starting to check them inbetween');
+           }
+
+           if(inBetween){
+               checkbox.checked = true;
+           }
+        });
+    }
+
+   lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
